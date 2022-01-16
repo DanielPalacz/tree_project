22631084c1e8a0f787acd273f19868b691870b25
DanielPalacz <daniel.palacz@gmail.com>
2022-01-16 13:10:59 +0100

    Doc and annotation updates - tree.py

--- tree.py
+++ tree.py
@@ -4,24 +4,55 @@ import os
 
 
 def get_sorted_subtitems(dirname: str = ".") -> List[str]:
-    return sorted([x.absolute() for x in Path(dirname).iterdir() if not x.is_dir()])
+    """
+    Description:
+    -- get not directory items present in the given directory
+    :param: dirname
+    :type dirname: str
+    :return: list of not directory items
+    :rtype: List[str]
+    """
+    return sorted([x for x in Path(dirname).iterdir() if not x.is_dir()])
 
 
 def get_sorted_dirs(dirname) -> List[str]:
+    """
+    Description:
+    -- get directories present in the given directory
+    :param: dirname
+    :type dirname: str
+    :return: list of directory items
+    :rtype: List[str]
+    """
     return sorted([x.absolute() for x in Path(dirname).iterdir() if x.is_dir()])
 
 
 def get_sorted_items(dirname: str = ".") -> List[str]:
+    """
+    Description:
+    -- get all items (directories and not) present in the given directory
+    :param: dirname
+    :type dirname: str
+    :return: list of directory items
+    :rtype: List[str]
+    """
     return get_sorted_subtitems(dirname) + get_sorted_dirs(dirname)
 
 
 def run(dirname: str = ".", indentation: int = 0) -> None:
+    """
+    Description:
+    -- run tree aplication
+    :param: dirname
+    :type dirname: str
+    :return: nothing
+    """
     os.chdir(dirname)
     for item_ in get_sorted_items(dirname):
         if not item_.is_dir():
-            print(" " * indentation, "-", item_.name.__str__())
+            print(" " * indentation, "-", item_.name)
         else:
-            print(" " * indentation, "-", item_.name.__str__() + "\\")
+            print(" " * indentation, "-", item_.name + "\\")
             indentation_level_up = indentation
-            run(item_.absolute().__str__(), indentation=indentation + len(item_.name.__str__()) + 2)
+            run(item_.absolute(), indentation=indentation + len(item_.name) + 2)
             indentation = indentation_level_up
